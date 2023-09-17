ea7eb1134f358eb572b0bcddeba4ef651471ff27
Mauro Hernandez <mauroDeveloper@hotmail.com>
2023-09-16 23:20:21 -0400

    reproductor de audio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,181 @@
+/* ************************** */
+/* ************************** */
+/* ************************** */
+/* MUSIC PLAYER */
+const MUTE = "musicPlayer-mute";
+const PLAYING = "musicPlayer-playing";
+const SPEED = "musicPlayer_changeSpeed-show";
+
+class MusicPlayer {
+  constructor() {
+    this.audio = document.getElementById("musicPlayer_audio");
+    this.btn_mute = document.getElementById("musicPlayer_mute");
+    this.playButtons = document.querySelectorAll(".musicPlayer_play");
+    this.btn_forward = document.getElementById("musicPlayer_forward");
+    this.btn_backward = document.getElementById("musicPlayer_backward");
+    this.volumeSlider = document.getElementById("musicPlayer_volumeSlider");
+    this.volumeBar = document.getElementById("musicPlayer_volumeBar");
+    this.progressInput = document.getElementById("musicPlayer_progress");
+    this.progresSlider = document.getElementById("musicPlayer_progresSlider");
+    this.currentTime = document.getElementById("musicPlayer_time-current");
+    this.totalTime = document.getElementById("musicPlayer_time-total");
+    this.speedButtons = document.querySelectorAll(".musicPlayer_changeSpeed");
+    this.image = document.querySelector(".musicPlayer_image");
+
+    this.loadActions();
+    this.loadInitData();
+  }
+
+  loadActions() {
+    this.btn_mute?.addEventListener("click", () => this.toggleMute());
+    this.btn_forward?.addEventListener("click", () => this.seekTime(15));
+    this.btn_backward?.addEventListener("click", () => this.seekTime(-15));
+    this.volumeSlider?.addEventListener("input", () => this.setVolume());
+    this.progressInput?.addEventListener("input", () => this.setProgress());
+
+    Array.from(this.playButtons).forEach((btn) => {
+      btn.addEventListener("click", () => this.togglePlay());
+    });
+
+    Array.from(this.speedButtons).forEach((btn) => {
+      btn.addEventListener("click", () => this.changeSpeed(btn.dataset.speed));
+    });
+  }
+
+  loadInitData() {
+    this.setVolume();
+    this.printTotalTime();
+  }
+
+  toggleMute() {
+    this.isMuted = !this.isMuted;
+    this.audio.muted = this.isMuted;
+    this.btn_mute?.classList.toggle(MUTE);
+  }
+
+  togglePlay() {
+    if (this.audio.paused) {
+      this.audio.play();
+      Array.from(this.playButtons).forEach((btn) => btn.classList.add(PLAYING));
+      this.audio.addEventListener("timeupdate", () => this.updateProgress());
+      this.image.classList.add(PLAYING);
+    } else {
+      this.audio.pause();
+      this.image.classList.remove(PLAYING);
+      Array.from(this.playButtons).forEach((btn) =>
+        btn.classList.remove(PLAYING)
+      );
+    }
+  }
+
+  setVolume() {
+    this.volume = parseFloat(this.volumeSlider.value);
+    this.audio.volume = this.volume;
+    this.volumeBar.style.width = `${this.volume * 100}%`;
+    this.setMute();
+  }
+
+  setMute(isMuted = false) {
+    this.audio.muted = isMuted;
+    this.isMuted = isMuted;
+
+    if (isMuted) {
+      this.btn_mute?.classList.add(MUTE, isMuted);
+    } else {
+      this.btn_mute?.classList.remove(MUTE, isMuted);
+    }
+  }
+
+  seekTime(seconds) {
+    this.audio.currentTime += seconds;
+  }
+
+  updateProgress() {
+    const currentTime = this.audio.currentTime;
+    const duration = this.audio.duration;
+
+    if (!isNaN(duration)) {
+      const percent = currentTime / duration;
+      this.progressInput.value = percent;
+      this.progresSlider.style.width = `${percent * 100}%`;
+      this.printCurrentTime();
+    }
+  }
+
+  setProgress() {
+    const percent = this.progressInput.value;
+    const duration = this.audio.duration;
+
+    if (!isNaN(duration)) {
+      const newPosition = percent * duration;
+      this.audio.currentTime = newPosition;
+      this.progresSlider.style.width = `${percent * 100}%`;
+    }
+  }
+
+  getTimeInMinutes(seconds) {
+    const totalMinutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    const formattedTime = `${totalMinutes
+      .toString()
+      .padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+    return formattedTime;
+  }
+
+  getTotalTimeFormatted() {
+    const duration = this.audio.duration;
+    if (!isNaN(duration)) {
+      return this.getTimeInMinutes(duration);
+    }
+    return "00:00";
+  }
+
+  getCurrentTimeFormatted() {
+    const currentTime = this.audio.currentTime;
+    return this.getTimeInMinutes(currentTime);
+  }
+
+  printCurrentTime() {
+    const currentTime = this.getCurrentTimeFormatted();
+    this.currentTime.innerHTML = currentTime;
+  }
+
+  printTotalTime() {
+    const totalTime = this.getTotalTimeFormatted();
+    this.totalTime.innerHTML = totalTime;
+  }
+
+  changeSpeed(speed) {
+    this.audio.playbackRate = parseFloat(speed);
+    this.hideAllChangeSpeed();
+
+    switch (speed) {
+      case "1.5":
+        this.speedButtons[1].classList.add(SPEED);
+        break;
+
+      case "2.0":
+        this.speedButtons[2].classList.add(SPEED);
+        break;
+
+      case "1.0":
+        this.speedButtons[0].classList.add(SPEED);
+        break;
+    }
+  }
+
+  hideAllChangeSpeed() {
+    Array.from(this.speedButtons).forEach((btn) => {
+      btn.classList.remove(SPEED);
+    });
+  }
+}
+
+/* MUSIC PLAYER */
+/* ************************** */
+/* ************************** */
+/* ************************** */
+
 function main() {
   /* ************************** */
   /* ************************** */
@@ -277,15 +455,16 @@ function main() {
   /* ************************** */
   /* ************************** */
   /* TRENDING TOPICS */
-  const trendingToìcSlider = document.querySelector('.trendingTopics_slider');
+  const trendingToìcSlider = document.querySelector(".trendingTopics_slider");
 
   if (trendingToìcSlider) {
     const slider = new Splide(trendingToìcSlider, {
-      arrowPath: 'M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z',
+      arrowPath:
+        "M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z",
       classes: {
-        arrow : 'splide__arrow trendingTopics-arrow',
-        prev  : 'splide__arrow--prev trendingTopics-prev',
-        next  : 'splide__arrow--next trendingTopics-next',
+        arrow: "splide__arrow trendingTopics-arrow",
+        prev: "splide__arrow--prev trendingTopics-prev",
+        next: "splide__arrow--next trendingTopics-next",
       },
       perPage: 5,
       perMove: 1,
@@ -302,13 +481,27 @@ function main() {
         },
         320: {
           perPage: 1,
-        }
-      }
+        },
+      },
     });
 
     slider.mount();
   }
   /* TRENDING TOPICS */
+  /* ************************** */
+  /* ************************** */
+  /* ************************** */
+
+  /* ************************** */
+  /* ************************** */
+  /* ************************** */
+  /* MUSIC PLAYER */
+  const music = document.getElementById("musicPlayer");
+
+  if (music) {
+    new MusicPlayer();
+  }
+  /* MUSIC PLAYER */
   /* ************************** */
   /* ************************** */
   /* ************************** */
